319cabf717736a64988d17fa8a5a89af7292f27f
Gerard Riera <gerardrp@ext.bershka.com>
2024-10-14 17:54:18 +0200

    test-2: add ui component

---
+++ app-rebase/TextView.swift
@@ -0,0 +1,14 @@
+//
+//  TextView.swift
+//  app-rebase
+//
+//  Created by Gerard Riera Puig on 14/10/24.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
