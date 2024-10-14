114bec1b9a08eb3c886b708f23e7c33c8e5935af
Gerard Riera <gerardrp@ext.bershka.com>
2024-10-14 17:52:25 +0200

    first commit

---
+++ app-rebase/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  app-rebase
+//
+//  Created by Gerard Riera Puig on 14/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ app-rebase/app_rebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_rebaseApp.swift
+//  app-rebase
+//
+//  Created by Gerard Riera Puig on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct app_rebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
