a02754f3869d05f3fbdd41431fedf96c4f59cc1c
Gerard Riera <gerardrp@ext.bershka.com>
2024-10-17 14:24:04 +0200

    test-3: Add MyView component

---
+++ app-rebase/MyView.swift
@@ -0,0 +1,14 @@
+//
+//  MyView.swift
+//  app-rebase
+//
+//  Created by Gerard Riera Puig on 17/10/24.
+//
+
+import SwiftUI
+
+struct MyView: View {
+    var body: some View {
+        Text("Hello World From MyView!")
+    }
+}
---
+++ app-rebase/MyViewTests.swift
@@ -0,0 +1,12 @@
+//
+//  MyViewTests.swift
+//  app-rebase
+//
+//  Created by Gerard Riera Puig on 17/10/24.
+//
+
+import Foundation
+
+class MyViewTests {
+    // Testing MyView Implementation
+}
